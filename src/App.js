587044dac627fefb4bb8cb9ab0765d7cde9ba83f
zach <zach@isolary.com>
2018-03-25 16:17:12 -0700

    fixed project tree & updated landing page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,22 @@ import React from 'react';
 import { hot } from 'react-hot-loader';
 import style from './App.scss';
 
-const App = () => <p className={style.hello}>Hello World</p>;
+const App = () => (
+  <div className={style.container}>
+    <div className={style.logo}>
+      <img
+        src="https://crows-nest.nyc3.digitaloceanspaces.com/assets/images/ada.png"
+        alt="Sneaky Crow"
+      />
+    </div>
+    <div className={style.mainItem} data-text="Sneaky Crow">
+      Sneaky Crow
+    </div>
+    <div>
+      <h1>Coming Soon</h1>
+      <a href="mailto:zach@sneakycrow.io">zach@sneakycrow.io</a>
+    </div>
+  </div>
+);
 
 export default hot(module)(App);
