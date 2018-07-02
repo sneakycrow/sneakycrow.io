d6655d1095472e156fdd23064241cc226d8e266c
Zachary E Sohovich <zacharyesohovich@gmail.com>
2018-07-02 11:50:54 -0700

    added second font and established defaults

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { injectGlobal } from 'emotion';
 import App from './components/App';
+
+injectGlobal`
+  body {
+    font-family: 'Source Sans Pro', sans-serif;
+  }
+  h1, h2, h3, h4, h5, h6 {
+    font-family: 'Lato', sans-serif;
+  }
+`;
 
 ReactDOM.render(<App />, document.getElementById('root'));
