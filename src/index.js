1c06e68b169cbbf5326e3220338b06af782a849d
Zachary E Sohovich <zacharyesohovich@gmail.com>
2018-07-04 14:05:00 -0700

    added global link styles

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { injectGlobal } from 'emotion';
-import { sizes } from 'assets/variables';
-import App from './components/App';
+import { sizes, pallette } from 'assets/variables';
+import { transparentize } from 'polished';
+import App from './App';
 
 injectGlobal`
   body {
     font-family: 'Source Sans Pro', sans-serif;
     font-size: ${sizes.font};
+    color: ${pallette.dark.variant1};
   }
   h1, h2, h3, h4, h5, h6 {
     font-family: 'Lato', sans-serif;
+  }
+  a {
+    text-decoration: none;
+    color: ${pallette.dark.primary};
+    &:hover {
+      color: ${pallette.white};
+      background-color: ${transparentize(0.6, pallette.dark.primary)};
+    }
   }
 `;
 
