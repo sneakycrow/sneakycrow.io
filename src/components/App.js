9a617d39c44df5836460c239a97ec18c88e0606d
Zachary E Sohovich <zacharyesohovich@gmail.com>
2018-07-04 10:31:38 -0700

    instantiated landing page

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,26 @@
 import React from 'react';
+import styled from 'react-emotion';
+import Avatar from 'components/Avatar';
+import { pallette } from 'variables';
 
-const App = () => <p>Hello world</p>;
+const { dark } = pallette;
+
+const Container = styled('div')`
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+  align-items: center;
+  justify-content: center;
+  background-color: ${dark.base};
+  flex-direction: column;
+  color: ${dark.variant2};
+`;
+
+const App = () => (
+  <Container>
+    Software Engineer
+    <Avatar imageSrc="https://crows-nest.nyc3.digitaloceanspaces.com/assets/images/ada.png" />
+  </Container>
+);
 
 export default App;
