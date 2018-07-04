1fcedaeafb88b71d4b627fc40c4a7a8372ad6327
Zachary E Sohovich <zacharyesohovich@gmail.com>
2018-07-04 10:30:39 -0700

    added universal font size

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { injectGlobal } from 'emotion';
+import { sizes } from 'variables';
 import App from './components/App';
 
 injectGlobal`
   body {
     font-family: 'Source Sans Pro', sans-serif;
+    font-size: ${sizes.font};
   }
   h1, h2, h3, h4, h5, h6 {
     font-family: 'Lato', sans-serif;
