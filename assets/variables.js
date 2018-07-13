758b27c5abc6d4df7a871b67f57cb837a27e0d8f
Zachary E Sohovich <zacharyesohovich@gmail.com>
2018-07-13 12:48:47 -0700

    added some super basic styling with emotion

---
+++ assets/variables.js
@@ -0,0 +1,12 @@
+export const pallette = {
+  white: '#FFFFFF',
+  black: '#000000'
+};
+
+export const spacing = {
+  xs: '8px',
+  sm: '16px',
+  md: '24px',
+  lg: '48px',
+  xlg: '64px'
+};
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,16 @@
-const App = () => <p>Hello World</p>;
+import { styled } from 'react-emotion';
+import { pallette } from '../assets/variables';
+
+const Container = styled('div')`
+  height: 100vh;
+  width: 100vw;
+  background-color: ${pallette.white};
+`;
+
+const App = () => (
+  <Container>
+    <p>Hello World</p>
+  </Container>
+);
 
 export default App;
